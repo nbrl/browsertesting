29237b3eeec29410d8b174edd859e766b4117e5b
Nathan Burrill <nathan@digitalanimal.com>
2015-03-05 11:52:10 +0000

    karma working :)

--- sdk.js
+++ sdk.js
@@ -1,6 +1,7 @@
 var AT = (function () {
     return {
         init: function (cb) {
+            console.log('IN HERE');
             cb(null);
         }
     };
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
-var sdk = require('./sdk');
+var sdk = require('../sdk');
 
 describe('first testing', function () {
     it('should run a test', function () {
