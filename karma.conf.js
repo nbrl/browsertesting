d59f630c50054744fbd605dd149c619fca97ea2a
Nathan Burrill <nathan@digitalanimal.com>
2015-04-02 15:34:22 +0100

    karma file test

--- karma.conf.js
+++ karma.conf.js
@@ -1,122 +1,113 @@
 // Karma configuration
-// Originally generated on Thu Mar 05 2015 11:02:25 GMT+0000 (GMT)
-// Will run against SauceLabs browsers from Travis CI, or against PhantomJS when
-// run locally :)
-
-var browsers = [
-  {
-    name: 'chrome',
-    versions: ['34', '35'],
-    platforms: ['Windows 7']
-  }
-];
+// Generated on Mon Mar 16 2015 11:11:15 GMT+0000 (GMT)
 
 module.exports = function(config) {
-
-  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-    console.log('Need to set SAUCE_USERNAME and SAUCE_ACCESS_KEY');
-    process.exit(1);
-  }
-
-  // Define SauceLabs browser matrix here
-  var customLaunchers = {
-    'SL_Chrome': {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '35'
+  // Require Sauce credentials on Travis
+  if (process.env.TRAVIS) {
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+      console.log('SauceLabs credentials are needed for Travis tests.');
+      process.exit(1);
     }
-  };
+  }
 
   var conf = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [
-      'mocha',
-      'browserify'
-    ],
+    frameworks: ['mocha', 'browserify'],
+
 
     // list of files / patterns to load in the browser
     files: [
-      'test/*.js'
+      './test/*.js'
     ],
+
 
     // list of files to exclude
     exclude: [
+      './**/*.swp'
     ],
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.js': ['browserify']
+      './test/*.js': ['browserify'],
     },
+
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    //reporters: ['progress'],
-    //reporters: ['spec'],
-    reporters: ['spec', 'saucelabs'],
+    // reporters: ['progress'],
+    reporters: ['spec'], // add 'saucelabs'
+
 
     // web server port
     port: 9876,
 
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    sauceLabs: {
-      testName: 'browser test test testing...',
-      recordScreenshots: false,
-      connectOptions: {
-        port: 5757,
-        logfile: 'sauce_connect.log'
-      }
-    },
-
-    // FIXME
-    //if (process.env.OSTYPE.match(/^darwin[0-9.]+/)) {
-      //// local
-      //browsers: ['PhantomJS'],
-    //} else {
-      //// Assume sauce labs
-      //// Increase timeout in case CI connection is slow
-      //captureTimeout: 120000,
-      //customLaunchers: customLaunchers,
-      //browsers: Object.keys(customLaunchers),
-    //}
-    //captureTimeout: 120000,
-    //customLaunchers: customLaunchers,
-    //browsers: Object.keys(customLaunchers),
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
+
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['PhantomJS', 'Chrome'],
-    //browsers: ['PhantomJS'],
+    browsers: ['PhantomJS'],
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false
   };
 
-
-  // FIXME!
-  if (process.env.ORCHESTRATION_HOME) {
-    conf.browsers = ['PhantomJS'];
-  } else {
+  // Set up Sauce Labs config when running on Travis
+  if (process.env.TRAVIS) {
+    conf.reporters = ['spec', 'saucelabs'];
+    conf.autoWatch = false;
+    conf.singleRun = true;
     conf.captureTimeout = 120000;
-    conf.customLaunchers = customLaunchers;
-    conf.browsers = Object.keys(customLaunchers);
+
+    conf.browsers = Object.keys(browsers);
+    conf.customLaunchers = browsers;
+
+    conf.sauceLabs = {
+      testName: 'advocate-things-js-sdk with Karma',
+      recordScreenshots: false,
+      connectoOptions: {
+        port: 5757,
+        logfile: 'sauce_connect.log'
+      }
+    };
   }
 
+  // This is the config that is passed to Karma
   config.set(conf);
+};
+
+var browsers = {
+  sl_chrome31_win7: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '31'
+  },
+  sl_ie7_winxp: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'windows xp',
+    version: '7.0'
+  }
 };
